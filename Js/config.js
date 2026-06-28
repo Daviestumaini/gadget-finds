cf9b23b32d70f0b4c1aef903e525144285e66591
Davies Tumaini <daviestumaini@gmail.com>
2026-06-28 22:14:08 +0300

    finalw

--- Js/config.js
+++ Js/config.js
@@ -1,9 +1,5 @@
-// Development
-const API_BASE = "http://localhost:5000/api";
 
-// Production (Render)
-// const API_BASE = "https://YOUR-BACKEND.onrender.com/api";
 const API_BASE =
     location.hostname === "localhost"
         ? "http://localhost:5000/api"
-        : "https://YOUR-RENDER-BACKEND.onrender.com/api";
+        : "https://gadget-finds.onrender.com";
